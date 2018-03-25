438fdd95f42da91f3758b65346f9d1d2545580c7
Séra Bálint <sera.balint@e-vista.hu>
2018-03-25 16:41:52 +0200

    roll dice

--- lib/service/dice_service.dart
+++ lib/service/dice_service.dart
@@ -1,13 +1,30 @@
+import 'dart:math';
+
 class DiceService {
   var members = new List<int>();
 
   DiceService(List<int> numbers) {
-    members.add(numbers[0]);
-    members.add(numbers[1]);
-    members.add(numbers[2]);
+    members.addAll(numbers);
   }
 
   List<int> throwDice() {
-    return members;
+    return _throwRandomly();
+  }
+
+  List<int> _throwRandomly() {
+    var rand = new Random();
+    int maxMember = members.length;
+    // Get the highest number from the list
+    print(maxMember.toString());
+    var randMembers = new List<int>();
+    for (var i = 0; i < members.length; i++) {
+      var random = rand.nextInt(maxMember);
+      // find an element that havent beed added to the new list
+      while (randMembers.indexOf(members[random]) > -1) {
+        random = rand.nextInt(maxMember);
+      }
+      randMembers.add(members[random]);
+    }
+    return randMembers;
   }
 }
--- lib/view/dice.dart
+++ lib/view/dice.dart
@@ -5,7 +5,7 @@ class Dice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var numbers = new List<int>();
-    numbers.addAll([1, 6, 5]);
+    numbers.addAll([1, 5, 12, 6, 7]);
     var diceService = new DiceService(numbers);
     return new Center(
       child: new Text('Dis is de dice: ' + diceService.throwDice().toString()),
